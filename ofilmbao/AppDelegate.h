355157100c14f8d6b442831491ec5bd9ffd769bf
susaaaan <878393556@qq.com>
2015-05-12 22:52:03 -0700

    Initial Commit

---
+++ ofilmbao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ofilmbao
+//
+//  Created by susan on 15-5-12.
+//  Copyright (c) 2015年 ofilm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ofilmbao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ofilmbao
+//
+//  Created by susan on 15-5-12.
+//  Copyright (c) 2015年 ofilm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
